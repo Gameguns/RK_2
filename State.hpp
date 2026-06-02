58385d0e8f8dcc05822673fc99d481a6cb227b10
Gameguns <kachanov.07@mail.ru>
2026-06-02 07:10:22 +0000

    Fix include guards and remove duplicate code

--- State.hpp
+++ State.hpp
@@ -1,24 +1,15 @@
 #ifndef STATE_HPP
 #define STATE_HPP
+
 #include <string>
 
 class State {
     std::string data;
 public:
-    State() : data("") {}  // ← конструктор по умолчанию
+    State() : data("") {}
     State(const std::string& s) : data(s) {}
     std::string getData() const { return data; }
     void setData(const std::string& s) { data = s; }
 };
 
-#endif#ifndef STATE_HPP
-#define STATE_HPP
-#include <string>
-class State {
-    std::string data;
-public:
-    State(const std::string& s) : data(s) {}
-    std::string getData() const { return data; }
-    void setData(const std::string& s) { data = s; }
-};
-#endif
+#endif 
