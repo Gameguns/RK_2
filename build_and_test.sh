46be94e29a9bea557aac31d3c97e455f338e038b
Gameguns <kachanov.07@mail.ru>
2026-06-02 07:52:58 +0000

    Clean build directory before each build to avoid CMake cache conflicts

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
+# Удаляем старую папку build
+rm -rf build
+
 mkdir -p build
 cd build
 cmake ..
 make
 
+# Запуск теста
 ./memento_test
 
+# Установка
+sudo make install || true
+
+# Упаковка
 sudo checkinstall --pkgname=memento-pattern --pkgversion=1.0 --backup=no --fstrans=no --install=no --nodoc -y
 
 mv *.deb ../
